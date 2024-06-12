04a4e9add192f4dc8ec3fd8c1861a072edfae5dc
Peter Hawkins <phawkins@google.com>
2024-06-12 13:02:44 -0700

    [XLA:CPU] Fix a wrong-output bug where the wrong initial value was being used for floating-point max collectives.
    Only affects the in-process collective implementation.
    
    Fixes https://github.com/google/jax/issues/21820
    
    PiperOrigin-RevId: 642709602

--- third_party/xla/xla/tests/collective_ops_test.cc
+++ third_party/xla/xla/tests/collective_ops_test.cc
@@ -151,6 +151,25 @@ class CollectiveOpsTest : public HloTestBase {
         "minimum",
         /*input_value=*/input_value.Clone(),
         /*expected_value=*/to_literal({cast(1), cast(2), cast(3)}));
+    if constexpr (std::numeric_limits<LiteralType>::is_signed) {
+      input_value = to_literal({cast(-1), cast(-2), cast(-3)});
+      TestTwoReplicasOneOperand<LiteralType>(
+          "add",
+          /*input_value=*/input_value.Clone(),
+          /*expected_value=*/to_literal({cast(-2), cast(-4), cast(-6)}));
+      TestTwoReplicasOneOperand<LiteralType>(
+          "multiply",
+          /*input_value=*/input_value.Clone(),
+          /*expected_value=*/to_literal({cast(1), cast(4), cast(9)}));
+      TestTwoReplicasOneOperand<LiteralType>(
+          "maximum",
+          /*input_value=*/input_value.Clone(),
+          /*expected_value=*/to_literal({cast(-1), cast(-2), cast(-3)}));
+      TestTwoReplicasOneOperand<LiteralType>(
+          "minimum",
+          /*input_value=*/input_value.Clone(),
+          /*expected_value=*/to_literal({cast(-1), cast(-2), cast(-3)}));
+    }
   }
 
  protected:
